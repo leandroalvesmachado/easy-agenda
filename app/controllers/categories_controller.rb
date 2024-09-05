f188ecc296681e53c9e4e17ec877c46e3a8b0994
leandroalvesmachado <leandroalvesmachado@gmail.com>
2024-09-05 15:38:18 -0300

    salvando

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,58 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: %i[ show edit update destroy ]
+
+  # GET /categories or /categories.json
+  def index
+    @categories = Category.order(:name)
+  end
+
+  # GET /categories/1 or /categories/1.json
+  def show
+  end
+
+  # GET /categories/new
+  def new
+    @category = Category.new
+  end
+
+  # GET /categories/1/edit
+  def edit
+  end
+
+  # POST /categories or /categories.json
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to categories_path, notice: "Categoria criada com sucesso."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /categories/1 or /categories/1.json
+  def update
+    if @category.update(category_params)
+      redirect_to categories_path, notice: "Categoria atualizada com sucesso."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /categories/1 or /categories/1.json
+  def destroy
+    @category.destroy!
+    redirect_to categories_path, notice: "Category deletada com sucesso."
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def category_params
+      params.require(:category).permit(:name)
+    end
+end
